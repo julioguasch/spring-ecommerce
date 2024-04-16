83180dee5e152626da31f3b955ac428a8b923c2e
julioguasch <julio.guasch.g@gmail.com>
2024-04-15 21:26:32 -0400

    guardar un producto

--- src/main/java/com/curso/ecommerce/springecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/springecommerce/controller/ProductoController.java
@@ -1,16 +1,48 @@
 package com.curso.ecommerce.springecommerce.controller;
 
+import com.curso.ecommerce.springecommerce.model.Producto;
+import com.curso.ecommerce.springecommerce.model.Usuario;
+import com.curso.ecommerce.springecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+
+    @Autowired
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
     @GetMapping("")
     public String show()
     {
         return "productos/show";
+    }
+
+    @GetMapping("/create")
+    public String create()
+    {
+        return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto)
+    {
+        LOGGER.info("Este es el objeto producto {}", producto);
+        Usuario usuario = new Usuario(1,"","","","","","","");
+        producto.setUsuario(usuario);
+        productoService.save(producto);
+        return "redirect:/productos";
     }
 }
